e364017d6eb3fe945d9b1fabf1f9318ba4643031
limorYavetz <anayavetz22@gmail.com>
2024-11-12 15:20:00 +0200

    add save uploaded image to database

--- router/multerRouterController.js
+++ router/multerRouterController.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const multer = require('multer');
 const path = require('path'); // Import the path module
+const YarnModel = require("../DB/mongodb/models/Yarn");
 
 const router = express.Router();
 
@@ -17,14 +18,38 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 // Handle image upload route
-router.post('/upload-image', upload.single('image'), (req, res) => {
+router.post('/upload-image', upload.single('image'), async (req, res) => {
     if (!req.file) {
         return res.status(400).json({ error: 'No image uploaded' });
     }
 
-    // Construct the image URL
+    console.log("router.post('/upload-image' req.body:", req.body);
+
+    // Image URL to be saved in the MongoDB database
     const imageUrl = `/images/${req.file.filename}`;
 
-    res.json({ imageUrl });  // Send back the image URL
+    // Assuming you're updating an existing yarn document with the image URL
+    const yarnId = req.body.yarnId;  // Get the yarnId from the request body (or params)
+
+    try {
+        console.log("router.post('/upload-image'. yarnId:", yarnId);
+        console.log("router.post('/upload-image'. imageUrl:", imageUrl);
+
+        // Find the yarn document and update it with the image URL
+        const updatedYarn = await YarnModel.findById(yarnId);
+        updatedYarn.image.imageurl = imageUrl;
+        console.log("router.post('/upload-image'. updatedYarn.image.imageurl:", updatedYarn.image.imageurl);
+        updatedYarn.save();
+        if (!updatedYarn) {
+            return res.status(404).send({ error: 'Yarn not found' });
+        }
+
+        // Send the updated image URL back in the response
+        res.send({ imageUrl });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ error: 'Internal server error' });
+    }
 });
+
 module.exports = router;
